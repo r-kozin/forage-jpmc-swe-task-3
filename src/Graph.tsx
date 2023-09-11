0d2cce98cec83d36aeb84baf9cc94c0a40b30470
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-09-11 14:59:35 -0700

    completed task 3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float', // Added 'price_abc' to the schema
+      price_def: 'float', // Added 'price_def' to the schema
+      ratio: 'float', // Added 'ratio' to the schema
       timestamp: 'date',
+      upper_bound: 'float', // Added 'upper_bound' to the schema
+      lower_bound: 'float', // Added 'lower_bound' to the schema
+      trigger_alert: 'float', // Added 'trigger_alert' to the schema
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,23 +39,23 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); // Added 'ratio', 'lower_bound', 'upper_bound', 'trigger_alert' to the columns
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg', // Added 'price_abc' to the aggregates
+        price_def: 'avg', // Added 'price_def' to the aggregates
+        ratio: 'avg', // Added 'ratio' to the aggregates
         timestamp: 'distinct count',
+        upper_bound: 'avg', // Added 'upper_bound' to the aggregates
+        lower_bound: 'avg', // Added 'lower_bound' to the aggregates
+        trigger_alert: 'avg', // Added 'trigger_alert' to the aggregates
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([DataManipulator.generateRow(this.props.data),] as unknown as TableData);
     }
   }
 }
